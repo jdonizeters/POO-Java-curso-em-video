64eeca8578c828023ba90c9af4e484d6e5084ddb
Donizete <jdonizeters@gmail.com>
2023-07-25 22:43:51 -0300

    Aula Prática 8 – Agregação entre Objetos com Java

--- UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
+++ UltraEmojiCombat/src/ultraemojicombat/UltraEmojiCombat.java
@@ -1,12 +1,14 @@
 package ultraemojicombat;
 
+import java.util.Random;
+
 public class UltraEmojiCombat {
 
     public static void main(String[] args) {
         Lutador lutadorUnico = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
         Lutador lutadores[] = new Lutador[6];
         
-        lutadorUnico.status();
+        //lutadorUnico.status();
         
         lutadores[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
         lutadores[1] = new Lutador("Putscripty", "Brasil", 29, 11.68f, 57.8f, 14, 2, 3);
@@ -15,7 +17,10 @@ public class UltraEmojiCombat {
         lutadores[4] = new Lutador ("UFOCobol","Brasil",37,1.70f,119.3f,5,4,3);
         lutadores[5] = new Lutador ("Nerdaart","EUA",30,1.81f,105.7f,12,2,4);
 
-        lutadores[5].status();
+        Luta UEC01 = new Luta();
+        
+        UEC01.marcarLuta(lutadores[0], lutadores[1]);
+        UEC01.lutar();
     }
     
 }
